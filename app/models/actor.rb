c0ad34440618bf10c6c1b060b64c2880e18ca0a5
Oscar Shoudt <shoudto@gmail.com>
2019-09-11 03:08:27 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -8,7 +8,7 @@ class Actor < ActiveRecord::Base
   end
 
   def list_roles
-    "#{self.characters.first.name}"
+    "#{self.characters.first.name} - #{self.shows.first.name}"
     binding.pry
   end
 end
